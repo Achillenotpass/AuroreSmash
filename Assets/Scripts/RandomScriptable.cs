2329bf33d56214f7b33bd552da7593b4f7f93a94
Achille PACE <achille.pace@laposte.net>
2021-04-06 12:04:12 +0200

    Created folders

---
+++ Assets/Scripts/RandomScriptable.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "RandomScriptable", menuName = "ScriptableObjects/RandomScriptable")]
+public class RandomScriptable : ScriptableObject
+{
+    
+    
+
+}
