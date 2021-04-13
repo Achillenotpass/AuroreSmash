1b1a6a09c2155a4d72cecbcc4d4965dae92bbbf2
Tom Mignotte <mignotte.tom@gmail.com>
2021-04-13 10:36:20 +0200

    StillTestMovement
    Don"t show interest it's just to pull

--- Assets/Scripts/TomTest/Movement.cs
+++ Assets/Scripts/TomTest/Movement.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Movement : MonoBehaviour
 {
@@ -19,5 +20,21 @@ public class Movement : MonoBehaviour
         m_PlayerDirection += new Vector3(0, -10 * Time.deltaTime, 0);
         m_CharacterController.Move(m_PlayerDirection);
         m_PlayerDirection = Vector3.zero;
+    }
+
+    private void PlayerMovement(InputAction.CallbackContext p_Context)
+    {
+        if(p_Context.started)
+        {
+
+        }
+        if(p_Context.performed)
+        {
+            m_PlayerDirection += new Vector3(p_Context.ReadValue<Vector2>().x, p_Context.ReadValue<Vector2>().y, 0);
+        }
+        if(p_Context.canceled)
+        {
+
+        }
     }
 }
